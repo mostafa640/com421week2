810961fb998131a4886d3aba29fa4b7373686831
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-05 22:58:35 +0100

    if statement com421

--- cat eat file/cat_eat.py
+++ cat eat file/cat_eat.py
@@ -9,7 +9,12 @@ class Cat:
 
 cat1 = Cat("Binnie", 4, 4)
 cat2 = Cat("Clyde", 1, 2)
-cat1.eat()
-cat2.eat()
+print("weight before eating")
 print(cat1.weight)
+print(cat2.weight)
+
+print("weight after eating")
+cat1.eat()
+print(cat1.weight)
+cat2.eat()
 print(cat2.weight)
